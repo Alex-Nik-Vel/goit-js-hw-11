dc2e3aad9603932289d48477918c81d0d999cac9
Alex-Nik-Vel <alex.nik.vel@gmail.com>
2022-10-25 20:35:38 +0300

    delete style.css

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -3,17 +3,14 @@
 
 import axios from 'axios';
 
-const BASE_URL = 'https://pixabay.com/api/';
-const API_KEY = '30755314-0b51072df48ae455376dac9e9';
-// const PER_PAGE = 40;
-
-
-
-export async function fetchImages(searchData, page) {
+export default async function fetchImages(searchData, page) {
+  const BASE_URL = 'https://pixabay.com/api/';
+  const API_KEY = '30755314-0b51072df48ae455376dac9e9';
+  const PER_PAGE = 40;
   // console.log('searchData===', searchData)
 
   const response = await axios.get(
-    `${BASE_URL}?key=${API_KEY}&q=${searchData}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40}`,
+    `${BASE_URL}?key=${API_KEY}&q=${searchData}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${PER_PAGE}}`,
   );
   return response;
 }
